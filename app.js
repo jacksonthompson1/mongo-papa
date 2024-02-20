e66732049d914904eee751df7d29a4a2fa8da077
Thompson <wthompson4@una.edu>
2024-02-20 16:43:09 -0600

    finally

--- app.js
+++ app.js
@@ -31,8 +31,12 @@ async function run() {
     // Send a ping to confirm a successful connection
     const result = await client.db("papa-database").collection("papa-collection").find().toArray();
 
+
       console.log("papadatabase:", result);
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+      return result;
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
